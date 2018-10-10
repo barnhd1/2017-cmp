2d5e0c2d1a4ffd00128a638f4f0ac22ca478f3fa
barnhd1 <42756301+barnhd1@users.noreply.github.com>
2018-10-10 15:20:12 -0700

    Update fourier.py

--- fourier.py
+++ fourier.py
@@ -3,24 +3,25 @@ import matplotlib.pyplot as plt
 
 data = np.loadtxt("circular.txt",float)
 
-l = np.fft.fft(data)
+transform = np.abs(np.fft.fft2(data))
+
+
 
 x = np.linspace(0.0,len(data), 501)
 
-plt.plot(data)
+plt.imshow(data)
 plt.title("Unprocessed")
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.savefig("first.png")
 plt.show()
 
-plt.plot(x,np.abs(l))
+plt.imshow(np.log(transform))
 plt.title("Processed")
 plt.xlabel('x')
 plt.ylabel('f(x)')
 plt.savefig("second.png")
 plt.show()
 
-
-print("The orginal graph was 485 kBs")
-print("The second file was 71 kBs")
+print(np.shape(data))
+print(np.shape(transform))
